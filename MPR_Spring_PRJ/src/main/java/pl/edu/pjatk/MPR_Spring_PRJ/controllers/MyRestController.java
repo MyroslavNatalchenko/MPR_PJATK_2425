90c6bb0f2e4291bc6b5df3c532b23e06a1b5e0ca
Myroslav Natalchenko <s30023@pjwstk.edu.pl>
2024-10-07 15:35:06 +0200

    Read info about Autowired

--- MPR_Spring_PRJ/src/main/java/pl/edu/pjatk/MPR_Spring_PRJ/controllers/MyRestController.java
+++ MPR_Spring_PRJ/src/main/java/pl/edu/pjatk/MPR_Spring_PRJ/controllers/MyRestController.java
@@ -1,5 +1,7 @@
 package pl.edu.pjatk.MPR_Spring_PRJ.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.pjatk.MPR_Spring_PRJ.model.School;
@@ -10,6 +12,11 @@ import java.util.List;
 @RestController
 public class MyRestController {
     private SchoolService schoolService;
+
+    @Autowired
+    public MyRestController(final SchoolService schoolService) {
+        this.schoolService = schoolService;
+    }
 
     @GetMapping("school/all")
     public List<School> getAll() {
